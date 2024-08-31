d7b218baff44994fa7fdf02d16be8ed4f2c66c88
Ariana <152944814+aam214@users.noreply.github.com>
2024-08-31 15:44:56 -0500

    In JS file added forEach method and map method to retrieve information from JSON of API. Used appendChild, classList.add, innerText and innerHTML to create the UI of the website.Changed header image and styling. Edited body background-color. Added class to ul element. Edited anchor text. Edited h2 css. Added class name instead of the element name in the css. Edited anchor CSS.  Adjusted image size, border-radius, and box-shadow.

--- recipe.js
+++ recipe.js
@@ -1,13 +1,44 @@
-const recipeList = document.getElementById(recipe-list);
+const recipeList = document.getElementById("recipe-list");
 
 
 function recipeView(recipes) {
 recipeList.innerHTML ="";
+recipes.forEach((recipe) => {
+  const recipeDetails = document.createElement("li");
+  recipeDetails.classList.add("recipe-details");
+  recipeImage = document.createElement("img");
+  recipeImage.src = recipe.image;
+  recipeImage.alt = "Recipe picture";
+
+  recipeName = document.createElement("h3");
+  recipeName.classList.add("recipe-name");
+  recipeName.innerHTML = recipe.title;
+
+  recipeDescription = document.createElement("p");
+  recipeDescription.classList.add("description");
+  recipeDescription.innerHTML = 
+  `Ingredients: ${recipe.extendedIngredients.map((ingredient) =>
+  ingredient.original).join(" -")}`;
+  
+  recipeButton = document.createElement("a");
+  recipeButton.href = recipe.sourceUrl;
+  recipeButton.innerText = "View Recipe";
+    recipeButton.classList.add("click");
+
+  
+  recipeDetails.appendChild(recipeName);
+  recipeDetails.appendChild(recipeImage);  
+  recipeDetails.appendChild(recipeButton)
+  recipeList.appendChild(recipeDetails);
+  recipeDetails.appendChild(recipeDescription);
+
+});
+
 }
 
 async function retrieveRecipes() {
-  const retrieved = 
-  await fetch(`https://api.spoonacular.com/recipes/random?number=9&apiKey=df8fc39572bb4cc6a7e799c975762126`);
+  const retrieved = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=9&apiKey=df8fc39572bb4cc6a7e799c975762126`);
   const data = await retrieved.json();
   return data.recipes;
 }
@@ -15,6 +46,7 @@ async function retrieveRecipes() {
 async function init() {
   const recipes = await retrieveRecipes();
   //console.log(recipes);
+  recipeView(recipes);
 }
 
 init();
